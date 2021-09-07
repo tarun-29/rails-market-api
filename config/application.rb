286b04b93ed784494717eaeea4bd889793883452
tarun-29 <29kantiwaltarun@gmail.com>
2021-09-08 00:03:02 +0530

    Setup token controller

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module MarketApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -31,11 +30,11 @@ module MarketApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.eager_load_paths << Rails.root.join('lib')
   end
 end
