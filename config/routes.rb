c652ea660e150f23486cfda37a1a71a113236b17
tarun-29 <29kantiwaltarun@gmail.com>
2021-09-10 22:20:45 +0530

    Adds the create method for the orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
       resources :products
-      resources :orders, only: %i[index show]
+      resources :orders, only: %i[index show create]
       # resources :products, only: %i[show index create update]
     # We're going to list our resources here.
     end
