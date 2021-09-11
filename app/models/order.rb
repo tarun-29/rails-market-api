9dd52f212ea9a9dff7443c2c8cafe6eb20b7f52b
tarun-29 <29kantiwaltarun@gmail.com>
2021-09-11 11:44:28 +0530

    Updates the total calculations for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,15 @@
 class Order < ApplicationRecord
+  include ActiveModel::Validations
   before_validation :set_total!
   belongs_to :user
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
   validates :total, numericality: {greater_than_or_equal_to: 0}
   validates :total, presence: true
+  validates_with EnoughProductsValidator
 
   def set_total!
-    self.total = products.map(&:price).sum
+    self.total = self.placements.map{|placement| placement.product.price * placement.quantity}.sum
   end
 
   def build_placements_with_product_ids_and_quantities (product_ids_and_quantities)
@@ -19,4 +21,5 @@ class Order < ApplicationRecord
       yield placement if block_given?
     end 
   end
+
 end
